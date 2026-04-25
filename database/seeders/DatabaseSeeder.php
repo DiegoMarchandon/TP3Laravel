9e8c80fc6f730370c984a7b30054aa05ecc1bce0
DiegoMarchandon <diegomarc2020@gmail.com>
2026-04-24 23:37:49 -0300

    modificando el seeder de post para arreglar imágenes de public storage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        // $this->call(UserSeeder::class);
-
-        
-        /* User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]); */
+        $this->call(ReaccionesSeeder::class);
+        User::factory(10)->create();
+        $this->call(PostSeeder::class);
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\User;
 
 class PostSeeder extends Seeder
 {
@@ -13,25 +14,30 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
+        $user = User::first();
+        
         Post::create([
             'title' => 'First Post',
-            'poster' => 'https://example.com/poster1.jpg',
+            'poster' => 'posters/5eioTRBGfZJLU1xXkCuP5IFIAzwXLz7MNbiF51eJ.png',
             'habilitated' => true,
             'content' => 'This is the content of the first post.',
+            'user_id' => $user->id,
         ]);
 
         Post::create([
             'title' => 'Second Post',
-            'poster' => 'https://example.com/poster2.jpg',
+            'poster' => 'posters/9oiz5O5eS5xMtO5mKjf2dBmZH1RuBnf3mDr9UczN.png',
             'habilitated' => false,
             'content' => 'This is the content of the second post.',
+            'user_id' => $user->id,
         ]);
 
         Post::create([
             'title' => 'Third Post',
-            'poster' => 'https://example.com/poster3.jpg',
+            'poster' => 'posters/A5VFYgbIdtMH4uvQipH58FWk82loSZOU5hE5br2i.png',
             'habilitated' => true,
             'content' => 'This is the content of the third post.',
+            'user_id' => $user->id,
         ]);
     }
 }
